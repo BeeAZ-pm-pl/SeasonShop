958eced476943c2ad6eb7a814b40316b6fb57cc3
Nguyễn Đạt <89953941+BeeAZZ@users.noreply.github.com>
2022-04-12 23:10:04 +0700

    Add files via upload

---
+++ src/BeeAZZ/SeasonShop/SeasonShop.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace BeeAZZ\SeasonShop;
+
+use pocketmine\player\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\item\{Item, ItemFactory, VanillaItems};
+use BeeAZZ\SeasonShop\libs\YTBJero\LibEconomy\Economy;
+use BeeAZZ\SeasonShop\libs\jojoe77777\FormAPI\SimpleForm;
+use BeeAZZ\SeasonShop\libs\jojoe77777\FormAPI\CustomForm;
+use pocketmine\utils\Config;
+use pocketmine\event\player\PlayerJoinEvent;
+
+
+class SeasonShop extends PluginBase implements Listener{
+  
+  protected $season = "";
+  
+  protected $price = "";
+  
+  protected $shop;
+  
+  protected $i;
+  
+  protected const VERSION = 1;
+  
+  protected const SHOPVERSION = 1;
+  
+  
+  public function onEnable() : void{
+   $this->getServer()->getPluginManager()->registerEvents($this, $this);
+   $this->saveDefaultConfig();
+   $this->saveResource("seasonshop.yml");
+   $this->shop = new Config($this->getDataFolder()."seasonshop.yml",Config::YAML);
+   if($this->getConfig()->get("version") !== self::VERSION){
+   $this->getLogger()->notice("§c§lPlease use the latest config");
+   $this->getServer()->getPluginManager()->disablePlugin($this);
+   }
+   if($this->shop->get("version") !== self::SHOPVERSION){
+   $this->getLogger()->notice("§c§lPlease use the latest seasonshop.yml");
+   $this->getServer()->getPluginManager()->disablePlugin($this);
+  }
+  }
+  
+  public function getSeason(){
+     date_default_timezone_set($this->getConfig()->get("date_default_timezone_set"));
+     $day = date("d");
+        if($day <= 31){
+            $season = "Last Winter";
+        }
+        if($day <= 29){
+            $season = "Winter";
+        }
+        if($day <= 24){
+            $season = "Last Autumn";
+        }
+        if($day <= 22){
+            $season = "Autumn";
+        }
+        if($day <= 14){
+            $season = "Last Summer";
+        }
+        if($day <= 12){
+            $season = "Summer";
+        }
+        if($day <= 7){
+            $season = "Last Spring";
+        }
+        if($day <= 5){
+            $season = "Spring";
+        }
+        return $season;
+    }
+  public function onJoin(PlayerJoinEvent $event){
+   $player = $event->getPlayer();
+   $this->getSeason(); #Checking the Season With Events Instead of Tasks Will Be Smoother
+   $player->sendMessage(str_replace("{Season}", $this->getSeason(), $this->getConfig()->get("Now-Season")));
+  }
+    
+  public function getSell(){
+   if($this->getSeason() == "Spring"){
+     $price = $this->getConfig()->get("Price-Spring");
+   }
+   if($this->getSeason() == "Summer"){
+     $price = $this->getConfig()->get("Price-Summer");
+   }
+   if($this->getSeason() == "Autumn"){
+     $price = $this->getConfig()->get("Price-Autumn");
+   }
+   if($this->getSeason() == "Winter"){
+     $price = $this->getConfig()->get("Price-Winter");
+   }
+    return $price;
+   }
+
+
+  public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool{
+   switch($cmd->getName()){
+    case "sshop":
+     $player = $sender;
+     if(!$player instanceof Player){
+      $player->sendMessage("Please Use Command In Game");
+      return true;
+     }
+     if($player->hasPermission("seasonshop.command")){
+     if($this->getSeason() !== "Spring" && $this->getSeason() !== "Summer" && $this->getSeason() !== "Autumn" && $this->getSeason() !== "Winter"){
+     $player->sendMessage($this->getConfig()->get("Last-Season"));
+     return true;
+  }
+   $this->shop($player);
+  }
+  break;
+   }
+   return true;
+  }
+  
+   public function shop($player){
+    $form = new SimpleForm(function(Player $player, $data){
+    if($data === null){
+     return true;
+   }
+    $id = $this->shop->get(strtolower($data))["id"];
+    $meta = $this->shop->get(strtolower($data))["meta"];
+    $amount = $this->shop->get(strtolower($data))["amount"];
+    $prices = $this->shop->get(strtolower($data))["price"];
+    $item = ItemFactory::getInstance()->get($id, $meta, $amount);
+    $price = $this->getSell() * $amount * $prices;
+    if(!$player->getInventory()->contains($item)){
+     $player->sendMessage($this->getConfig()->get("No-Item-Sell"));
+     return true;
+    }
+     $player->getInventory()->removeItem($item);
+     Economy::addMoney($player, $price);
+     $player->sendMessage(str_replace("{PRICE}", $price, $this->getConfig()->get("Sell-MSG")));
+    });
+    for($i = 0; $i <= 100; $i++){
+    if($this->shop->exists($i)){
+     $form->addButton($this->shop->get(strtolower($i))["name"]);
+    }
+    }
+    $form->setTitle($this->getConfig()->get("UI")["Title"]);
+    $form->setContent(str_replace("{SEASON}", $this->getSeason(), $this->getConfig()->get("UI")["Content"]));
+    $form->sendToPlayer($player);
+    }
+   }
---
+++ src/BeeAZZ/SeasonShop/libs/YTBJero/LibEconomy/Economy.php
@@ -0,0 +1,65 @@
+<?php
+
+#Thank For Eric!
+
+declare(strict_types=1);
+
+namespace BeeAZZ\SeasonShop\libs\YTBJero\LibEconomy;
+
+use pocketmine\Server as PMServer;
+use pocketmine\player\Player;
+
+final class Economy {
+
+	private const ECONOMYAPI = "EconomyAPI";
+	
+	private const BEDROCKECONOMYAPI = "BedrockEconomyAPI";
+
+	/**
+	 * @return array
+	 */
+	private static function getEconomy() {
+		$api = PMServer::getInstance()->getPluginManager()->getPlugin("EconomyAPI");
+		if($api !== null){
+			return [self::ECONOMYAPI, $api];
+		} else{
+			$api = PMServer::getInstance()->getPluginManager()->getPlugin("BedrockEconomy");
+			if($api !== null){
+				return [self::BEDROCKECONOMYAPI, $api];
+			}
+		}
+        	throw new \Exception("You not have economy plugin.");
+	}
+	/**
+	 * @param  Player $player
+	 */
+	public static function myMoney(Player $player){
+		if(self::getEconomy()[0] === self::ECONOMYAPI){
+			return self::getEconomy()[1]->myMoney($player);
+		} elseif(self::getEconomy()[0] === self::BEDROCKECONOMYAPI){
+			return self::getEconomy()[1]->getAPI()->getPlayerBalance($player->getName());
+		}
+	}
+	/**
+	 * @param Player $player
+	 * @param int $amount
+	 */
+	public static function addMoney(Player $player, $amount){
+		if(self::getEconomy()[0] === self::ECONOMYAPI){
+			self::getEconomy()[1]->addMoney($player, $amount);
+		} elseif(self::getEconomy()[0] === self::BEDROCKECONOMYAPI){
+			return self::getEconomy()[1]->getAPI()->addToPlayerBalance($player->getName(), (int) ceil($amount));
+		}
+	}
+	/**
+	 * @param  Player $player
+	 * @param  int $amount
+	 */
+	public static function reduceMoney(Player $player, $amount){
+		if(self::getEconomy()[0] === self::ECONOMYAPI){
+			self::getEconomy()[1]->reduceMoney($player, $amount);
+		} elseif(self::getEconomy()[0] === self::BEDROCKECONOMYAPI){
+			return self::getEconomy()[1]->getAPI()->subtractFromPlayerBalance($player->getName(), (int) ceil($amount));
+		}
+	}
+}
